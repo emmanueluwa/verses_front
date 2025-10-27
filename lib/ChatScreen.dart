cf79c96b60b889907dbfec4c03194d6349043f14
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2025-10-27 14:38:49 +0000

    adding config for llm call

--- lib/ChatScreen.dart
+++ lib/ChatScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -9,6 +10,30 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  askLLM(){
+    post(Uri.parse("https://api.openai.com/v1/chat/completions"),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ${String.fromEnvironment('OPENAI_API_KEY')}'
+    },
+
+    body: {
+    "model": "gpt-4o-mini",
+    "messages": [
+    {
+    "role": "developer",
+    "content": "You are a helpful assistant."
+    },
+    {
+    "role": "user",
+    "content": inputController.text
+    }
+    ]
+    });
+  }
+
+  TextEditingController inputController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,8 +43,11 @@ class _ChatScreenState extends State<ChatScreen> {
           Expanded(child: Center(child: Text("Response shown here..."))),
           Row(
             children: [
-              Expanded(child: TextField()),
-              IconButton(onPressed: (){},icon:Icon(Icons.send))
+              Expanded(child: TextField(controller: inputController)),
+              IconButton(onPressed: (){
+                askLLM();
+
+              },icon:Icon(Icons.send))
             ],
           )
         ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:verse_front/ChatScreen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load();
   runApp(const MyApp());
 }
 
