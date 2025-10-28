7b9ec1b53fb54b9dbe120ae99e8215543a12f5a1
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2025-10-28 06:12:32 +0000

    updating search bar ui

--- lib/ChatScreen.dart
+++ lib/ChatScreen.dart
@@ -11,32 +11,29 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-
   var llmResponseText = "Response to query will be shown here...";
 
   String get openaiAPIKey => dotenv.env['OPENAI_API_KEY'] ?? "";
 
   askLLM() async {
-    final response = await post(Uri.parse("https://api.openai.com/v1/chat/completions"),
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer $openaiAPIKey'
-    },
+    final response = await post(
+      Uri.parse("https://api.openai.com/v1/chat/completions"),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $openaiAPIKey',
+      },
 
-    body: jsonEncode({
-      "model": "gpt-4o-mini",
-      "messages": [
-        {
-          "role": "developer",
-          "content": "You are a helpful assistant."
-        },
-        {
-          "role": "user",
-          "content": inputController.text
-        }
-      ]
-    }));
-    
+      body: jsonEncode({
+        "model": "gpt-4o-mini",
+        "messages": [
+          {"role": "developer", "content": "You are a helpful assistant."},
+          {"role": "user", "content": inputController.text},
+        ],
+      }),
+    );
+
+    inputController.text = "";
+
     if (response.statusCode == 200) {
       final jsonData = jsonDecode(response.body);
       llmResponseText = jsonData["choices"][0]["message"]["content"];
@@ -54,23 +51,36 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.amber,),
+      appBar: AppBar(backgroundColor: Colors.amber),
       body: Column(
         children: [
           Expanded(child: Center(child: Text(llmResponseText))),
-          Row(
-            children: [
-              Expanded(child: TextField(controller: inputController)),
-              IconButton(onPressed: (){
-                print("object");
-                askLLM();
-                print("done");
-
-              },icon:Icon(Icons.send))
-            ],
-          )
+          Card(
+            color: Colors.white,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+            margin: EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 15.0),
+                    child: TextField(
+                      controller: inputController,
+                      decoration: InputDecoration(border: InputBorder.none, hintText: "What's on your mind?"),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    askLLM();
+                  },
+                  icon: Icon(Icons.send),
+                ),
+              ],
+            ),
+          ),
         ],
-      )
+      ),
     );
   }
 }
